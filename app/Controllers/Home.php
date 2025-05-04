4b189a537ca4387faf527a545daacaca7c0d10aa
Arjuna Bimantara <arjunabimantara912GMAIL.COM>
2025-05-05 03:02:56 +0700

    Update front-end v0.1

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\Home_Model;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('front_end/home');
+
+        $productModel = new Home_Model();
+        $data['products'] = $productModel->findAll();
+        return view('front_end/home', $data);
     }
 }
